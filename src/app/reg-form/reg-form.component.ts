fac2459c69bef0c25ef96878957ee44912309742
praharsha8325 <praharshadevarakonda@gmail.com>
2021-06-28 11:24:24 +0530

    files added

---
+++ src/app/reg-form/reg-form.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'reg-form',
+  templateUrl: './reg-form.component.html',
+  styleUrls: ['./reg-form.component.css']
+})
+export class RegFormComponent {
+  log(x: any) {console.log(x);}
+}
